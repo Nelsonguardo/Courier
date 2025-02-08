ea49b54ed124b89a0f168335220969a24f82521e
Nelsonguardo <guardonei4@gmail.com>
2025-02-08 18:15:51 -0500

    Validaciones y automatizacion para la asignacion de rutas

--- controllers/shipmentAssignmentController.js
+++ controllers/shipmentAssignmentController.js
@@ -1,14 +1,31 @@
 const shipmentAssignmentModel = require('../models/shipmentAssignmentModel.js');
 
+// Crear una nueva asignación de envío
 const createShipmentAssignment =  async (req, res) => {
     try {
-        const { shipment_id, carrier_id, vehicle_id, route_id} = req.body;
+        const { shipment_id, carrier_id, vehicle_id} = req.body;
 
-        if (!shipment_id || !carrier_id || !vehicle_id || !route_id) {
+        // Validar datos
+        if (!shipment_id || !carrier_id || !vehicle_id) {
             return res.status(400).json({ error: 'Faltan datos por enviar' });
         }
-
-        const result = await shipmentAssignmentModel.createShipmentAssignment(shipment_id, carrier_id, vehicle_id, route_id);
+        // Verificar si el envío ya fue asignado
+        const assignmentExists = await shipmentAssignmentModel.shipmentAssignmentExists(shipment_id);
+        if (assignmentExists.length > 0) {
+            return res.status(409).json({ error: 'Envío ya asignado' });
+        }
+        // Verificar si el envío existe
+        const oneShipment = await shipmentAssignmentModel.findOneShipment(shipment_id);
+        if (oneShipment.length === 0) {
+            return res.status(404).json({ error: 'Envío no encontrado' });
+        }
+        // Verificar si la ruta existe
+        const oneRoute = await shipmentAssignmentModel.findRoute(oneShipment[0].origin_city, oneShipment[0].destination_city);
+        if (oneRoute.length === 0) {
+            return res.status(404).json({ error: 'Ruta no encontrada' });
+        }
+        // Crear la asignación de envío
+        const result = await shipmentAssignmentModel.createShipmentAssignment(shipment_id, carrier_id, vehicle_id, oneRoute[0].id);
         res.status(200).json(result);
     } catch (error) {
         console.error(error);
--- models/shipmentAssignmentModel.js
+++ models/shipmentAssignmentModel.js
@@ -1,5 +1,6 @@
 const createConnection = require('../dataBase/db');
 
+// Crear una nueva asignación de envío
 const createShipmentAssignment = async (shipment_id, carrier_id, vehicle_id, route_id) => {
     const connection = await createConnection();
     const [result] = await connection.execute(
@@ -10,6 +11,42 @@ const createShipmentAssignment = async (shipment_id, carrier_id, vehicle_id, rou
     return result;
 };
 
+// Encontrar una ruta
+const findRoute = async (origin_city, destination_city) => {
+    const connection = await createConnection();
+    const [result] = await connection.execute(
+        "SELECT * FROM routes WHERE origin_city = ? AND destination_city = ?;",
+        [origin_city, destination_city]
+    );
+    await connection.end();
+    return result;
+};
+
+// Encontrar un envío
+const findOneShipment = async (shipment_id) => {
+    const connection = await createConnection();
+    const [result] = await connection.execute(
+        "SELECT * FROM shipments WHERE id = ?;",
+        [shipment_id]
+    );
+    await connection.end();
+    return result;
+};
+
+// Verificar si una asignación de envío ya existe
+const shipmentAssignmentExists = async (shipment_id) => {
+    const connection = await createConnection();
+    const [result] = await connection.execute(
+        "SELECT * FROM shipment_assignments WHERE shipment_id = ?;",
+        [shipment_id]
+    );
+    await connection.end();
+    return result;
+};
+
 module.exports = {
-    createShipmentAssignment
+    createShipmentAssignment,
+    findRoute,
+    findOneShipment,
+    shipmentAssignmentExists
 };
