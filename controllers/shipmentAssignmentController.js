25caaa4ea3751bc750339deee7091f97ca65e642
Nelsonguardo <guardonei4@gmail.com>
2025-02-08 21:38:40 -0500

    Funciones para validar disponibilidad de tranportista y vehiculo

--- controllers/shipmentAssignmentController.js
+++ controllers/shipmentAssignmentController.js
@@ -1,32 +1,63 @@
 const shipmentAssignmentModel = require('../models/shipmentAssignmentModel.js');
+const ShipmentModel = require('../models/shipmentModel');
 
 // Crear una nueva asignación de envío
 const createShipmentAssignment =  async (req, res) => {
     try {
-        const { shipment_id, carrier_id, vehicle_id} = req.body;
+        const { shipment_id, carrier_id, vehicle_id } = req.body;
 
         // Validar datos
         if (!shipment_id || !carrier_id || !vehicle_id) {
             return res.status(400).json({ error: 'Faltan datos por enviar' });
         }
+
         // Verificar si el envío ya fue asignado
         const assignmentExists = await shipmentAssignmentModel.shipmentAssignmentExists(shipment_id);
         if (assignmentExists.length > 0) {
             return res.status(409).json({ error: 'Envío ya asignado' });
         }
+
         // Verificar si el envío existe
         const oneShipment = await shipmentAssignmentModel.findOneShipment(shipment_id);
         if (oneShipment.length === 0) {
             return res.status(404).json({ error: 'Envío no encontrado' });
         }
+
         // Verificar si la ruta existe
         const oneRoute = await shipmentAssignmentModel.findRoute(oneShipment[0].origin_city, oneShipment[0].destination_city);
         if (oneRoute.length === 0) {
             return res.status(404).json({ error: 'Ruta no encontrada' });
         }
+
+        // Verificar si el transportista y el vehículo existen
+        const validateCarrierAndVehicle = await shipmentAssignmentModel.validateCarrierAndVehicle(carrier_id , vehicle_id);
+        if (validateCarrierAndVehicle.length === 0) {
+            return res.status(404).json({ error: 'Transportista y vehículo no vinculados a la ruta' });
+        }
+    
+        // Verificar si el transportista está disponible
+        const availableCarriers = await shipmentAssignmentModel.availableCarriers(carrier_id);
+        if (availableCarriers.length === 0) {
+            return res.status(404).json({ error: 'Transportista no disponible' });
+        }
+
+        // Verificar si el vehículo está disponible
+        const availableVehicles = await shipmentAssignmentModel.availableVehicles(vehicle_id);
+        let current_weight = availableVehicles[0].current_weight + oneShipment[0].weight;;
+        let max_weight = availableVehicles[0].max_weight;
+        if (availableVehicles.length > 0) {
+            if (current_weight > max_weight) {
+                return res.status(404).json({ error: 'Vehículo no cuenta con suficiente capacidad' });
+            }
+        }
+
         // Crear la asignación de envío
         const result = await shipmentAssignmentModel.createShipmentAssignment(shipment_id, carrier_id, vehicle_id, oneRoute[0].id);
-        res.status(200).json(result);
+        if (result.affectedRows > 0) {
+            await ShipmentModel.updateShipmentStatus(shipment_id, 'en tránsito', 'Envío asignado a transportista');
+            return res.status(201).json({ message: 'Asignación de envío creada exitosamente' });
+        }
+        
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Error al crear la asignación de envío' });
--- models/shipmentAssignmentModel.js
+++ models/shipmentAssignmentModel.js
@@ -44,9 +44,55 @@ const shipmentAssignmentExists = async (shipment_id) => {
     return result;
 };
 
+// Verificar si un transportista está disponible
+const availableCarriers = async (carrier_id) => {
+    const connection = await createConnection();
+    const [result] = await connection.execute(
+        "SELECT * FROM carriers WHERE id = ? AND available = 1;",
+        [carrier_id]
+    );
+    await connection.end();
+    return result;
+};
+
+// Verificar si un vehículo está disponible
+const availableVehicles = async (vehicle_id) => {
+    const connection = await createConnection();
+    const [result] = await connection.execute(
+        `
+       SELECT SUM(shipments.weight) AS current_weight ,vehicles.capacity as max_weight
+        FROM shipment_assignments
+        INNER JOIN  shipments ON shipments.id = shipment_assignments.shipment_id
+        INNER JOIN vehicles ON vehicles.id = shipment_assignments.vehicle_id
+        WHERE vehicles.id = ?;
+        `,
+        [vehicle_id]
+    );
+    await connection.end();
+    return result;
+};
+
+const validateCarrierAndVehicle = async (carrier_id, vehicle_id) => {
+    const connection = await createConnection();
+    const [result] = await connection.execute(
+        `
+          SELECT * FROM carriers 
+            INNER JOIN vehicles ON vehicles.carrier_id = carriers.id
+            INNER JOIN routes ON routes.id = carriers.route_id
+            WHERE carriers.id = ? AND vehicles.id = ?;
+         `,
+        [carrier_id, vehicle_id]
+    );
+    await connection.end();
+    return result;
+};
+
 module.exports = {
     createShipmentAssignment,
     findRoute,
     findOneShipment,
-    shipmentAssignmentExists
+    shipmentAssignmentExists,
+    availableCarriers,
+    availableVehicles,
+    validateCarrierAndVehicle
 };
